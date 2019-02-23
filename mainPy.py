abb149e5538cb004e8133c0f86e4e2f7dde0f585
SebastianG343 <juan.gelvez.2018@upb.edu.co>
2019-02-23 10:50:49 -0500

    Mi primer commit

---
+++ mainPy.py
@@ -0,0 +1,6 @@
+print("Digite dos numeros para calcular el producto de la multilicación entre ellos, Pls que sean enteros")
+a=int(input("Digite un numero"))
+c=int(input("Digite un numero para multiplicarlo con el numero anterirormente digitado"))
+lel=a*c 
+doble=a*2
+print("El producto de la multiplicación de",a,"y",c,"es",lel,"y el doble del numero",a,"es",doble)
